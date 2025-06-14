b8dffb75a2abeb805949dc8da7364d9304790c3f
Kiyoshi Nagahama <kiyoshi@cyberseeds.com>
2025-06-14 12:19:19 -0600

    Updates

---
+++ assets/GroupBox.stories-BEFmvJ87.js
@@ -0,0 +1,6 @@
+import{j as s}from"./iframe-C6JWnM9V.js";import{G as a}from"./GroupBox-Dw6E2UCX.js";import"./Label-CJ4EUNuc.js";import"./clsx-B-dksMZM.js";const m={component:a,title:"System/GroupBox",tags:["autodocs"],argTypes:{label:{control:{type:"text"}}}},n=s.jsx("div",{children:"This is inside of the group box."}),o={args:{label:"Label",children:n}};var e,r,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    label: "Label",
+    children: groupBoxContent
+  }
+}`,...(t=(r=o.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const u=["Default"];export{o as Default,u as __namedExportsOrder,m as default};
--- assets/GroupBox.stories-C8zFnVJI.js
+++
@@ -1,6 +0,0 @@
-import{j as s}from"./iframe-SMyN5pTS.js";import{G as a}from"./GroupBox-uFXLZpXd.js";import"./Label-CMijhWPq.js";import"./clsx-B-dksMZM.js";const m={component:a,title:"System/GroupBox",tags:["autodocs"],argTypes:{label:{control:{type:"text"}}}},n=s.jsx("div",{children:"This is inside of the group box."}),o={args:{label:"Label",children:n}};var e,r,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    label: "Label",
-    children: groupBoxContent
-  }
-}`,...(t=(r=o.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const u=["Default"];export{o as Default,u as __namedExportsOrder,m as default};
