47777786e9704c8eab6dcfc5671b83eed837ed76
Kiyoshi Nagahama <kiyoshi@cyberseeds.com>
2025-06-17 21:31:58 -0600

    Updates

---
+++ assets/GroupBox.stories-Dr_4s4vb.js
@@ -0,0 +1,7 @@
+"use client";
+import{j as s}from"./iframe-BPlIpqzu.js";import{G as a}from"./GroupBox-DG3nISXo.js";import"./Label-Dhcx-Mlm.js";import"./clsx-BYFsuUQf.js";const m={component:a,title:"System/GroupBox",tags:["autodocs"],argTypes:{label:{control:{type:"text"}}}},n=s.jsx("div",{children:"This is inside of the group box."}),o={args:{label:"Label",children:n}};var e,r,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    label: "Label",
+    children: groupBoxContent
+  }
+}`,...(t=(r=o.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const u=["Default"];export{o as Default,u as __namedExportsOrder,m as default};
---
+++ assets/PhoneInput.stories-rAkDG1tT.js
@@ -0,0 +1,14 @@
+"use client";
+import{r as m,j as p}from"./iframe-BPlIpqzu.js";import{P as s}from"./PhoneInput-CKRG3xwV.js";import"./useUIColor-DJw1Sg90.js";import"./Label-Dhcx-Mlm.js";import"./clsx-BYFsuUQf.js";const h={component:s,title:"System/PhoneInput",tags:["autodocs"],argTypes:{label:{control:{type:"text"}},isInvalid:{control:{type:"boolean"}},disabled:{control:{type:"boolean"}}}},e={args:{label:"電話番号",scale:"md",color:"blue",isInvalid:!1,disabled:!1},render:a=>{const[t,l]=m.useState("");return p.jsx(s,{value:t,onChange:l,...a})}};var o,r,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    label: "電話番号",
+    scale: "md",
+    color: "blue",
+    isInvalid: false,
+    disabled: false
+  },
+  render: args => {
+    const [phoneNyumber, setPhoneNumber] = useState<string>("");
+    return <PhoneInput value={phoneNyumber} onChange={setPhoneNumber} {...args} />;
+  }
+}`,...(n=(r=e.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const g=["Basic"];export{e as Basic,g as __namedExportsOrder,h as default};
