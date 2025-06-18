1583537b071b4d512187474b89c9baa4f4d0cc25
Kiyoshi Nagahama <kiyoshi@cyberseeds.com>
2025-06-17 21:02:46 -0600

    Updates

---
+++ assets/Input.stories-BJk56qMd.js
@@ -0,0 +1,45 @@
+"use client";
+import{j as s}from"./iframe-8uhpACiu.js";import{U as y}from"./useUIColor-gKLwgPHU.js";import{I}from"./Input-4Wx5FM2B.js";import"./Label-_p8NY5se.js";import"./clsx-BYFsuUQf.js";const j={component:I,title:"System/Input",tags:["autodocs"],argTypes:{label:{control:{type:"text"}},scale:{control:{type:"radio"},options:["sm","md"]},color:{control:{type:"select"},options:["red","orange","amber","yellow","lime","green","emerald","teal","cyan","sky","blue","indigo","violet","purple","fuchsia","pink","gray","zinc","neutral","stone"]},isInvalid:{control:{type:"boolean"}},disabled:{control:{type:"boolean"}}}},e={args:{label:"メールアドレス",scale:"md",color:"blue",placeholder:"メールアドレス",require:!1,isInvalid:!1,disabled:!1},render:v=>s.jsx(y,{initialColor:"gray",children:s.jsx(I,{...v})})},r={args:{label:"メールアドレス",scale:"md",color:"blue",placeholder:"メールアドレス",require:!0,isInvalid:!1,disabled:!1}},a={args:{label:"メールアドレス",scale:"md",color:"blue",placeholder:"メールアドレス",require:!1,isInvalid:!0,disabled:!1}},l={args:{label:"メールアドレス",scale:"md",color:"blue",placeholder:"メールアドレス",require:!1,isInvalid:!1,disabled:!0}};var o,n,i;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    label: "メールアドレス",
+    scale: "md",
+    color: "blue",
+    placeholder: "メールアドレス",
+    require: false,
+    isInvalid: false,
+    disabled: false
+  },
+  render: args => <UIColorProvider initialColor="gray">
+      <Input {...args} />
+    </UIColorProvider>
+}`,...(i=(n=e.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var d,t,c;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: "メールアドレス",
+    scale: "md",
+    color: "blue",
+    placeholder: "メールアドレス",
+    require: true,
+    isInvalid: false,
+    disabled: false
+  }
+}`,...(c=(t=r.parameters)==null?void 0:t.docs)==null?void 0:c.source}}};var u,p,m;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    label: "メールアドレス",
+    scale: "md",
+    color: "blue",
+    placeholder: "メールアドレス",
+    require: false,
+    isInvalid: true,
+    disabled: false
+  }
+}`,...(m=(p=a.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var b,f,g;l.parameters={...l.parameters,docs:{...(b=l.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    label: "メールアドレス",
+    scale: "md",
+    color: "blue",
+    placeholder: "メールアドレス",
+    require: false,
+    isInvalid: false,
+    disabled: true
+  }
+}`,...(g=(f=l.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};const D=["Default","Require","Invalid","Disabled"];export{e as Default,l as Disabled,a as Invalid,r as Require,D as __namedExportsOrder,j as default};
---
+++ assets/Switch.stories-25cc3MD8.js
@@ -0,0 +1,18 @@
+"use client";
+import{j as a,r as l}from"./iframe-8uhpACiu.js";import{S as o}from"./Switch-DzGhvKkb.js";import"./clsx-BYFsuUQf.js";import"./useUIColor-gKLwgPHU.js";const k={component:o,title:"System/Switch",tags:["autodocs"],argTypes:{scale:{control:{type:"radio"},options:["sm","md"]},color:{control:{type:"select"},options:["red","orange","amber","yellow","lime","green","emerald","teal","cyan","sky","blue","indigo","violet","purple","fuchsia","pink","gray","zinc","neutral","stone"]},disabled:{control:{type:"boolean"}}}},p=e=>{const[s,t]=l.useState(!1);return a.jsx(o,{...e,checked:s,onClick:()=>t(!s),onLabel:"オン",offLabel:"オフ"})},r={args:{scale:"md",color:"blue",disabled:!1},render:e=>a.jsx(p,{...e})},c={render:()=>{const[e,s]=l.useState(!0),[t,h]=l.useState(!0);return a.jsxs("div",{className:"cs:grid cs:grid-cols-2",children:[a.jsx(o,{checked:e,onClick:()=>s(!e),onLabel:"オン",offLabel:"オフ"}),a.jsx(o,{scale:"sm",checked:t,onClick:()=>h(!t),onLabel:"オン",offLabel:"オフ"})]})}};var n,i,d;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    scale: "md",
+    color: "blue",
+    disabled: false
+  },
+  render: args => <BasicSwitch {...args} />
+}`,...(d=(i=r.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var m,S,u;c.parameters={...c.parameters,docs:{...(m=c.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => {
+    const [standardSwitch, setStandardSwitch] = useState<boolean>(true);
+    const [smallSwitch, setSmallSwitch] = useState<boolean>(true);
+    return <div className="cs:grid cs:grid-cols-2">
+        <Switch checked={standardSwitch} onClick={() => setStandardSwitch(!standardSwitch)} onLabel="オン" offLabel="オフ" />
+        <Switch scale="sm" checked={smallSwitch} onClick={() => setSmallSwitch(!smallSwitch)} onLabel="オン" offLabel="オフ" />
+      </div>;
+  }
+}`,...(u=(S=c.parameters)==null?void 0:S.docs)==null?void 0:u.source}}};const y=["Primary","Scale"];export{r as Primary,c as Scale,y as __namedExportsOrder,k as default};
