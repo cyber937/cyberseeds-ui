9feed5e76c02394913fabd68f5a6d6fc79a986e9
Kiyoshi Nagahama <kiyoshi@cyberseeds.com>
2026-02-09 09:25:00 -0700

    Updates

---
+++ assets/Modal.stories-BcgDBa9d.js
@@ -0,0 +1,17 @@
+import{j as a}from"./iframe-Ldy2bjAX.js";import{B as r}from"./Button-GmtaSlb-.js";import{M as e}from"./Modal-bZJZ-hT4.js";import"./colorMap-DhHElisi.js";const p={component:e,title:"System/Modal",argTypes:{width:{control:{type:"radio"},options:["sm","md","lg"]}}},o={args:{width:"md"},render:t=>a.jsx("div",{className:"cs:h-full",children:a.jsxs(e,{...t,children:[a.jsx(e.Header,{children:"Modal"}),a.jsx(e.Body,{children:"Sample modal body sentencse."}),a.jsx(e.Footer,{children:a.jsxs("div",{className:"cs:flex cs:gap-2",children:[a.jsx(r,{variant:"secondary",children:"キャンセル"}),a.jsx(r,{children:"確定"})]})})]})})};var s,d,n;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    width: "md"
+  },
+  render: args => <div className="cs:h-full">
+      <Modal {...args}>
+        <Modal.Header>Modal</Modal.Header>
+        <Modal.Body>Sample modal body sentencse.</Modal.Body>
+        <Modal.Footer>
+          <div className="cs:flex cs:gap-2">
+            <Button variant="secondary">キャンセル</Button>
+            <Button>確定</Button>
+          </div>
+        </Modal.Footer>
+      </Modal>
+    </div>
+}`,...(n=(d=o.parameters)==null?void 0:d.docs)==null?void 0:n.source}}};const x=["Primary"];export{o as Primary,x as __namedExportsOrder,p as default};
---
+++ assets/PillBox.stories-D0AawkUw.js
@@ -0,0 +1,42 @@
+import{j as l}from"./iframe-Ldy2bjAX.js";import{P as e}from"./PillBox-BEPBqeLC.js";import"./clsx-B-dksMZM.js";const P={component:e,title:"System/PillBox",tags:["autodocs"],argTypes:{label:{control:"text"},scale:{control:{type:"radio"},options:["sm","md"]},color:{control:{type:"select"},options:["red","orange","amber","yellow","lime","green","emerald","teal","cyan","sky","blue","indigo","violet","purple","fuchsia","pink","gray","zinc","neutral","stone"]}}},o={args:{label:"PillBox",color:"red",scale:"md",className:""}},r={render:()=>l.jsx("div",{className:"cs:space-y-4",children:l.jsxs("div",{className:"cs:grid cs:grid-cols-5 cs:gap-6",children:[l.jsx(e,{label:"Red",color:"red"}),l.jsx(e,{label:"Orange",color:"orange"}),l.jsx(e,{label:"Amber",color:"amber"}),l.jsx(e,{label:"Yellow",color:"yellow"}),l.jsx(e,{label:"Lime",color:"lime"}),l.jsx(e,{label:"Green",color:"green"}),l.jsx(e,{label:"Emerald",color:"emerald"}),l.jsx(e,{label:"Teal",color:"teal"}),l.jsx(e,{label:"Cyan",color:"cyan"}),l.jsx(e,{label:"Sky",color:"sky"}),l.jsx(e,{label:"Blue",color:"blue"}),l.jsx(e,{label:"Indigo",color:"indigo"}),l.jsx(e,{label:"Violet",color:"violet"}),l.jsx(e,{label:"Purple",color:"purple"}),l.jsx(e,{label:"Fuchsia",color:"fuchsia"}),l.jsx(e,{label:"Pink",color:"pink"}),l.jsx(e,{label:"Rose",color:"rose"}),l.jsx(e,{label:"Slate",color:"slate"}),l.jsx(e,{label:"Gray",color:"gray"}),l.jsx(e,{label:"Zinc",color:"zinc"}),l.jsx(e,{label:"Neutral",color:"neutral"}),l.jsx(e,{label:"Stone",color:"stone"})]})})},a={render:()=>l.jsx("div",{className:"cs:space-y-4",children:l.jsxs("div",{className:"cs:grid cs:grid-cols-5 cs:gap-6",children:[l.jsx(e,{label:"Red",color:"red",scale:"md"}),l.jsx(e,{label:"Red",color:"red",scale:"sm"})]})})};var s,c,n;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    label: "PillBox",
+    color: "red",
+    scale: "md",
+    className: ""
+  }
+}`,...(n=(c=o.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};var i,d,t;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: () => <div className="cs:space-y-4">
+      <div className="cs:grid cs:grid-cols-5 cs:gap-6">
+        <PillBox label="Red" color="red" />
+        <PillBox label="Orange" color="orange" />
+        <PillBox label="Amber" color="amber" />
+        <PillBox label="Yellow" color="yellow" />
+        <PillBox label="Lime" color="lime" />
+        <PillBox label="Green" color="green" />
+        <PillBox label="Emerald" color="emerald" />
+        <PillBox label="Teal" color="teal" />
+        <PillBox label="Cyan" color="cyan" />
+        <PillBox label="Sky" color="sky" />
+        <PillBox label="Blue" color="blue" />
+        <PillBox label="Indigo" color="indigo" />
+        <PillBox label="Violet" color="violet" />
+        <PillBox label="Purple" color="purple" />
+        <PillBox label="Fuchsia" color="fuchsia" />
+        <PillBox label="Pink" color="pink" />
+        <PillBox label="Rose" color="rose" />
+        <PillBox label="Slate" color="slate" />
+        <PillBox label="Gray" color="gray" />
+        <PillBox label="Zinc" color="zinc" />
+        <PillBox label="Neutral" color="neutral" />
+        <PillBox label="Stone" color="stone" />
+      </div>
+    </div>
+}`,...(t=(d=r.parameters)==null?void 0:d.docs)==null?void 0:t.source}}};var x,b,m;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: () => <div className="cs:space-y-4">
+      <div className="cs:grid cs:grid-cols-5 cs:gap-6">
+        <PillBox label="Red" color="red" scale="md" />
+        <PillBox label="Red" color="red" scale="sm" />
+      </div>
+    </div>
+}`,...(m=(b=a.parameters)==null?void 0:b.docs)==null?void 0:m.source}}};const j=["Default","Color","Scale"];export{r as Color,o as Default,a as Scale,j as __namedExportsOrder,P as default};
